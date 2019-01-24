b7df3ca48607c9294aaab66974405196fa115b31
David Hodge <dchodge1@pstcc.edu>
2019-01-24 16:43:35 -0500

    initial commit

---
+++ CISP1010/pp1/pp1a.cpp
@@ -0,0 +1,11 @@
+/*
+ * File: pp1a.cpp
+ * Author: David Hodge
+ * This program am gets 2 integers from the user, adds them and displays the sum
+ */
+#include <iostream>
+
+int main()
+{
+	return (0);
+}
---
+++ CISP1010/pp1/pp1b.cpp
@@ -0,0 +1,11 @@
+/*
+ * File: pp1b.cpp
+ * Author: David Hodge
+ * This program calculates forces given mass and acceleration
+ */
+#include <iostream>
+
+int main()
+{
+	return (0);
+}
