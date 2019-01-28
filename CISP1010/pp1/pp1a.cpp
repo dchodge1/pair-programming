821953a6a590ced5758c6402a36c04ddf5b2999d
David Hodge <dchodge1@pstcc.edu>
2019-01-27 19:24:41 -0500

    Adding compiled files

--- CISP1010/pp1/pp1a.cpp
+++ CISP1010/pp1/pp1a.cpp
@@ -1,12 +1,32 @@
 /*
- * File: pp1a.cpp
- * Author: David Hodge
- * This program am gets 2 integers from the user, adds them and displays the sum
- */
+ * PP1a.cpp
+ * Stephen Souther
+ * January 25, 2019
+ * Calculates the sum, difference, product, and quotient of number1 and number2
+ * */	
+
 #include <iostream>
+
 using namespace std;
 
-int main()
-{
-	return (0);
+int main(){
+	int number1, number2;
+	int sum, diff, prod, quot;
+
+	cout << "Enter first number: ";
+	cin >> number1;
+
+	cout << "Enter second number: ";
+	cin >> number2;
+
+	sum = number1 + number2;
+	diff = number1 - number2;
+	prod = number1 * number2;
+	quot = number1 / number2;
+
+	cout << "The sum is: " << sum << endl;
+	cout << "The difference is: " << diff << endl;
+	cout << "The product is: " << prod << endl;
+	cout << "The quotient is: " << quot << endl;
+	return(0);
 }
