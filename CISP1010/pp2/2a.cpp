8bacc954a9fd33a9e2246dac33b9a4838634a8ab
David Hodge <dchodge1@pstcc.edu>
2019-02-04 11:17:23 -0500

    Fix typo in quote for 2a

--- CISP1010/pp2/2a.cpp
+++ CISP1010/pp2/2a.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 int main() {
 	double x = 42.1298;
-	cout 	<< "The answer to question of\n"
+	cout 	<< "The answer to the question of\n"
 		<< "Life, the Universe, and Everything is ";
 
 	cout.setf(ios::fixed);
