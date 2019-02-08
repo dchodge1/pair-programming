be6f70d06456c96227e0df107561b80ce653d749
snsouther <prowler@TerminalForce.org>
2019-02-08 11:32:17 -0500

    source and program for problem 3d

---
+++ CISP1010/pp3/3d.cpp
@@ -0,0 +1,46 @@
+/*
+	3c.cpp
+	Stephen Souther
+	February 9, 2019
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	char input;
+
+	do{
+		cout << "Enter a menu selection: ";
+		cin >> input;
+
+		switch(input){
+			case 'h':
+			case 'H':
+				cout << "You ordered hardwood" << endl;
+				break;
+			case 'l':
+			case 'L':
+				cout << "You ordered laminate" << endl;
+				break;
+			case 'e':
+			case 'E':
+				cout << "You ordered engineered hardwood" << endl;
+				break;
+			case 'Q':
+			case 'q':
+				break;
+			default:
+				cout << "Invalid choice" << endl;
+		}
+
+		if(input != 'Q' && input != 'q'){
+			cout << "H hardwood \nL laminate \nE engineered hardwood \nQ quit" << endl;
+		}
+
+	} while(input != 'Q' && input != 'q');
+
+	return(0);
+}
