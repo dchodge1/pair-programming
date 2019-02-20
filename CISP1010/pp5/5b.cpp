32289929e7f63bfb680b76ae46529daa8edf4eb7
snsouther <prowler@TerminalForce.org>
2019-02-20 11:10:00 -0500

    problem 5b solution

---
+++ CISP1010/pp5/5b.cpp
@@ -0,0 +1,37 @@
+/*
+	5b.cpp
+	Stephen Souther
+	February 20, 2019
+*/
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void figurelt(double in){
+	if(fmod(in,2) == 0){
+		cout << "even, ";
+	}
+	else{
+		cout << "not even, ";
+	}
+
+	if(in >= 0){
+		cout << "+, ";
+	}
+	else{
+		cout << "-, ";
+	}
+
+	cout << round(in) << endl;
+}
+
+int main(){
+	double input;
+
+	cout << "Please enter a number: ";
+	cin >> input;
+
+	figurelt(input);
+}
