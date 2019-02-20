575df7ec1fb7c53abca7cbc3bf52fb6eb25be89d
David Hodge <dchodge1@pstcc.edu>
2019-02-20 11:01:00 -0500

    Adding 5a.cpp

---
+++ CISP1010/pp5/5a.cpp
@@ -0,0 +1,33 @@
+/*
+ * Author: David Hodge
+ * File: 5a.cpp
+ * Date: 2019/02/20
+ * Program to swap values using reference variables
+ */
+
+#include <iostream>
+using namespace std;
+
+/*
+ * first: reference variable to swap with second
+ * second: reference variable to swap with first
+ * Post-condition: values of first and second are swapped
+ * returns nothing
+ */
+void myswap(int& first,int& second);
+
+int main() {
+    int x, y;
+    cout << "Please enter two integers seperated by a space: ";
+    cin >> x >> y;
+    cout << "Pre-swap x = " << x << " and y = " << y << endl;
+    myswap(x, y);
+    cout << "Post-swap x = " << x << " and y = " << y << endl;
+    return 0;
+}
+
+void myswap(int& first, int& second) {
+    int temp = first;
+    first = second;
+    second = temp;
+}
