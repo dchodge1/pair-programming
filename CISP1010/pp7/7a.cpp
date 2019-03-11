eeca062be16a01cc6592bbf7a2c65ca12f3fdd6e
David Hodge <dchodge1@pstcc.edu>
2019-03-11 11:21:11 -0400

    Adding 7a.cpp

---
+++ CISP1010/pp7/7a.cpp
@@ -0,0 +1,34 @@
+/*
+ * Author: David Hodge
+ * File: 7a.cpp
+ * Date: 2019/03/11
+ * Program to calculate average grade given an array of grades
+ */
+
+#include <iostream>
+using namespace std;
+
+//case(s): 1,2
+//const int MAX_STUDENTS = 5;
+
+//case(s): 3
+const int MAX_STUDENTS = 6;
+
+int main() {
+    //case 1
+    //double grades[MAX_STUDENTS] = {99.9,65.7,87.8,96.1,56.8};
+    //case 2
+    //double grades[MAX_STUDENTS] = {56.8,65.7,87.8,96.1,99.9};
+    //case 3
+    double grades[MAX_STUDENTS] = {56.8,65.7,87.8,88.8,96.1,99.9};
+    double total, average;
+    for (int i = 0; i < MAX_STUDENTS; i++) {
+       total += grades[i];
+    }
+    average = total/MAX_STUDENTS;
+    cout.setf(ios::showpoint);
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout << "Average: " << average << endl;
+    return 0;
+}
