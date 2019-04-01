362868c4d02b28f9c07ba85549ec3ef8e9ab6702
snsouther <prowler@TerminalForce.org>
2019-04-01 11:02:48 -0400

    solution to problem 8b

---
+++ CISP1010/pp8/8b.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+void myStrcpy(char[], char[]);
+
+int main(){
+
+	char a[25], b[25];
+
+	cout << "Enter a string: ";
+	cin >> a;
+
+	myStrcpy(a, b);
+
+	cout << "Second string: " << b << endl;
+
+	return(0);
+}
+
+void myStrcpy(char first[], char second[]){
+	for(int i = 0; i < strlen(first)+1; i++){
+		second[i] = first[i];
+	}
+}
